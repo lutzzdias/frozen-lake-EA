e91c45df00b0ea30334de8407c1a52c1663ce1f5
lutzzdias <thiagolutzdias@hotmail.com>
2024-05-05 21:10:13 +0100

    refactor: rename maps and move constants to different file
    this commit by itself is not "runable", it requires changes to the main
    file

---
+++ constants.py
@@ -0,0 +1,15 @@
+# Number of generations
+MAX_ITERATIONS_4X4 = 100
+MAX_ITERATIONS_8X8 = 200
+MAX_ITERATIONS_12X12 = 500
+
+# Number of individuals
+POPULATION_SIZE = 100
+
+# Variation probabilities
+CROSSOVER_PROBABILITY = 0.8
+MUTATION_PROBABILITY = 0.05
+
+# Parent selection
+TOURNAMENT_SIZE = 5
+ELITE_PERCENTAGE = 0.05  # 5% of the population
--- maps.py
+++ maps.py
@@ -3,9 +3,3 @@ from gymnasium.envs.toy_text.frozen_lake import generate_random_map
 map_4_by_4 = generate_random_map(size=4, seed=42)
 map_8_by_8 = generate_random_map(size=8, seed=42)
 map_12_by_12 = generate_random_map(size=12, seed=42)
-
-
-MAX_ITERATIONS_4_by_4 = 100
-MAX_ITERATIONS_8_by_8 = 200
-MAX_ITERATIONS_12_by_12 = 500
-
