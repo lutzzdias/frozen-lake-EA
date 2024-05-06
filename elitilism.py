eba95c088c1c9618e06f2bb41f77296efc0dedc7
lutzzdias <thiagolutzdias@hotmail.com>
2024-05-06 20:20:57 +0100

    feat: implement elitilist survivor selection

---
+++ elitilism.py
@@ -0,0 +1,9 @@
+from constants import ELITE_PERCENTAGE
+
+
+def elitilist_survivors(parents, offspring):
+    population_size = len(parents)
+    elite_size = int(population_size * ELITE_PERCENTAGE)
+    parents.sort(key=lambda individual: individual.fitness_value, reverse=True)
+    new_population = parents[:elite_size] + offspring[: population_size - elite_size]
+    return new_population
