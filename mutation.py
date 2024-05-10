6f9bcd72503e6f8fd78fd2275c19f6c86a28e2b1
lutzzdias <thiagolutzdias@hotmail.com>
2024-05-10 20:53:34 +0100

    refactor: simplify mutation (only change value)

--- mutation.py
+++ mutation.py
@@ -8,40 +8,15 @@ def should_mutate():
     return random.random() < MUTATION_PROBABILITY
 
 
-def append_decision(genotype):
-    decision = random.randint(0, 3)
-    genotype.append(decision)
-    return genotype
-
-
-def delete_decision(genotype):
-    index = random.randint(0, len(genotype) - 1)
-    genotype.pop(index)
-    return genotype
-
-
-def change_decision(genotype):
-    index = random.randint(0, len(genotype) - 1)
-    decision = random.randint(0, 3)
-
-    while decision == genotype[index]:
-        decision = random.randint(0, 3)
-
-    genotype[index] = decision
-    return genotype
-
-
 def mutate(individual: Individual):
-    genotype = individual.genotype
+    index = random.randint(0, len(individual.genotype))
 
-    mutations = [
-        append_decision(genotype),
-        delete_decision(genotype),
-        change_decision(genotype),
-    ]
+    options = [0, 1, 2, 3]
+    options.remove(individual.genotype[index])
 
-    mutation = random.choice(mutations)
-    individual.genotype = mutation
+    individual.genotype[index] = random.choice(options)
+
+    # recalculate fitness
     individual.traverse_maze(ENV)
 
     return individual
